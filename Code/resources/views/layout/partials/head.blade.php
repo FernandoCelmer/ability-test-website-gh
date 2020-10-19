6bd0d8983b95b7dbf90ddc6c78de047a99e70184
Fernando Celmer <email@fernandocelmer.com>
2020-10-19 00:07:23 -0300

    Implementação Template Bootstrap

---
+++ Code/resources/views/layout/partials/head.blade.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Album example for Bootstrap</title>
+<!-- Bootstrap core CSS -->
+<link href="https://getbootstrap.com/docs/4.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<!-- Custom styles for this template -->
+<link href="/css/album.css" rel="stylesheet">
